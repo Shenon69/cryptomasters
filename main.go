6500f26e418e69f9fec3414e213d62fa45c7a399
Shenon69 <ask.tikki69@gmail.com>
2024-04-16 10:46:40 +0530

    feat: Go routines with waiting groups

--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/Shenon69/cryptomasters/api"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	currencies := []string{"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func (currencyCode string){
+			getCurrencyData(currencyCode)
+			wg.Done()
+		}(currency)
+	}
+
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
 
 	if err == nil {
-		fmt.Printf("The rate for %v is %.2f", rate.Currency, rate.Price)
+		fmt.Printf("The rate for %v is %.2f \n", rate.Currency, rate.Price)
 	}
 }
